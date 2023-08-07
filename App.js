fda9c6fc046aeb8c61fa949510e865e1c3de913f
Nicole Slater <nicoleslater@pursuit.org>
2023-08-07 19:34:02 -0400

    Working in App.js

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+const App = () => <StudentList />;
+
+
+const StudentList  = () => {
+    return <Student />
+  
+};
+
+const Student = () => {
+  return <div>Student component</div>;
+  
+};
+
+
+
+export default App;
