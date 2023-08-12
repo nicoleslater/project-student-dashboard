341a049eca2c81fd12dfe9707f3bf983bd1d2333
Nicole Slater <nicoleslater@pursuit.org>
2023-08-12 11:32:05 -0400

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './index.css';
-import data from "./data/data.json";
+import dataJson from "./data/data.json";
+
+
+ const useStudentList = () => {
+  const url = "https://xsgames.co/randomusers/avatar.php?g=male&minimum_age=38&maximum_age=48";
+  const [allStudents, setAllStudents] = useState([]);
+  const [displayedStudents, setDisplayedStudents] = useState([]);
+
+  const getNewStudents = async () => {
+    const studentData = await fetch(url);
+    const studentJson = await dataJson();
+    setAllStudents(dataJson.className);
+    setDisplayedStudents(dataJson.cohort);
+  }
+ }
 
 // function App() {
   
@@ -18,24 +32,30 @@ import data from "./data/data.json";
 // const App = () => <StudentList />;
 
 
-function App(){
-const Student  = () => {
+// function App(){
 
-console.log(data[0].cohort.cohortStartDate);
-console.log(data[3]);
+useEffect(() => {
+  alert("Please Try Again!")
+  getNewStudents()
+})
+
+// const Student  = () => {
+
+// console.log(data[0].cohort.cohortStartDate);
+// console.log(data[3]);
 
  
-    return (
-      <div className="cohort">
-        <p>
-          All Students By: {data.filter("cohortCode")};
-          <User />
-        </p>
+//     return (
+//       <div className="cohort">
+//         <p>
+//           All Students By: {data.filter("cohortCode")};
+//           <User />
+//         </p>
 
-      </div>
-    );
-}
-}
+//       </div>
+//     );
+// }
+// }
   
 // };
 
@@ -46,60 +66,60 @@ console.log(data[3]);
 // }
 
 
-const StudentList = () => {
+// const StudentList = () => {
  
   
-  const url = "https://xsgames.co/randomusers/avatar.php?g=male&minimum_age=38&maximum_age=48";
-  return ( 
-  <><div className="student">
-      <p> Cohort: {data[0].cohort.cohortCode} </p>
-      <img src={url} atl="" />
-      <div>
-        <h4>Name: {data[0].names.preferredName}
-        </h4>
-        <h4>Email: {data[0].username}</h4>
-        <h4>Birthday: {data[0].dob}</h4>
-      </div>
-    </div><div className="student">
-        <p> Cohort: {data[1].cohort.cohortCode} </p>
-        <img src={url} atl="" />
-        <div>
-          <h4>Name: {data[1].names.preferredName}
-          </h4>
-          <h4>Email: {data[1].username}</h4>
-          <h4>Birthday: {data[1].dob}</h4>
-        </div>
-      </div><div className="student">
-        <p> Cohort: {data[2].cohort.cohortCode} </p>
-        <img src={url} atl="" />
-        <div>
-          <h4>Name: {data[2].names.preferredName}
-          </h4>
-          <h4>Email: {data[2].username}</h4>
-          <h4>Birthday: {data[2].dob}</h4>
-        </div>
-      </div><div className="student">
-        <p> Cohort: {data[3].cohort.cohortCode} </p>
-        <img src={url} atl="" />
-        <div>
-          <h4>Name: {data[3].names.preferredName}
-          </h4>
-          <h4>Email: {data[3].username}</h4>
-          <h4>Birthday: {data[3].dob}</h4>
-        </div>
-      </div><div className="student">
-        <p> Cohort: {data[4].cohort.cohortCode} </p>
-        <img src={url} atl="" />
-        <div>
-          <h4>Name: {data[4].names.preferredName}
-          </h4>
-          <h4>Email: {data[4].username}</h4>
-          <h4>Birthday: {data[4].dob}</h4>
-        </div>
-      </div></>
-  );
+//   const url = "https://xsgames.co/randomusers/avatar.php?g=male&minimum_age=38&maximum_age=48";
+//   return ( 
+//   <><div className="student">
+//       <p> Cohort: {data[0].cohort.cohortCode} </p>
+//       <img src={url} atl="" />
+//       <div>
+//         <h4>Name: {data[0].names.preferredName}
+//         </h4>
+//         <h4>Email: {data[0].username}</h4>
+//         <h4>Birthday: {data[0].dob}</h4>
+//       </div>
+//     </div><div className="student">
+//         <p> Cohort: {data[1].cohort.cohortCode} </p>
+//         <img src={url} atl="" />
+//         <div>
+//           <h4>Name: {data[1].names.preferredName}
+//           </h4>
+//           <h4>Email: {data[1].username}</h4>
+//           <h4>Birthday: {data[1].dob}</h4>
+//         </div>
+//       </div><div className="student">
+//         <p> Cohort: {data[2].cohort.cohortCode} </p>
+//         <img src={url} atl="" />
+//         <div>
+//           <h4>Name: {data[2].names.preferredName}
+//           </h4>
+//           <h4>Email: {data[2].username}</h4>
+//           <h4>Birthday: {data[2].dob}</h4>
+//         </div>
+//       </div><div className="student">
+//         <p> Cohort: {data[3].cohort.cohortCode} </p>
+//         <img src={url} atl="" />
+//         <div>
+//           <h4>Name: {data[3].names.preferredName}
+//           </h4>
+//           <h4>Email: {data[3].username}</h4>
+//           <h4>Birthday: {data[3].dob}</h4>
+//         </div>
+//       </div><div className="student">
+//         <p> Cohort: {data[4].cohort.cohortCode} </p>
+//         <img src={url} atl="" />
+//         <div>
+//           <h4>Name: {data[4].names.preferredName}
+//           </h4>
+//           <h4>Email: {data[4].username}</h4>
+//           <h4>Birthday: {data[4].dob}</h4>
+//         </div>
+//       </div></>
+//   );
     
-};
+// };
 
 /* 
 // class UserList extends React.Component {
